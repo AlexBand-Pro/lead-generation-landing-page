0f85a53037800988f1be21b7eb5e128e9c4d6d0d
AlexBand-Pro <tillromutni@gmail.com>
2025-04-20 23:54:20 -0600

    finish section one

---
+++ index.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+  document.getElementById("intro--client-name").classList.add("visible")
+  document.querySelector("h1").classList.add("visible")
+}, 600)
+
+setTimeout(() => {
+  document.getElementById("intro--client-hero-text").classList.add("visible")
+}, 1100)
+
+setTimeout(() => {
+  document.getElementById("hero-img").classList.add("visible")
+}, 1600)
